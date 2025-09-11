c3327048f77a85476da3f646474885a848b05de7
All-00k <ay879018@gmail.com>
2025-09-11 15:43:49 +0530

    NEW sync

---
+++ Shape.java
@@ -0,0 +1,39 @@
+public class Shape {
+    public static void main(String[] args) {
+        Rectangle obj1 = new Rectangle();
+        
+        System.out.println("Area of circle :" + obj1.Area(9, 19));
+        System.out.println("Perimeter of Rectangle : " + obj1.Perimeter(9,19));
+
+
+        Circle obj2 = new Circle();
+        System.out.println("Area of Circle " + obj2.Area(4));
+        System.out.println("Perimeter of Circle : " + obj2.Perimeter(4));
+
+        
+    }
+    
+}
+class Rectangle{
+    public int Area(int l,int b){
+        return l * b;
+
+    }
+    public int Perimeter(int l, int b){
+        return 2*(l+b) ;
+    }
+}
+
+class Circle{
+    public double Area(int r){
+        return r*r;
+
+    }
+    public double Perimeter(int r){
+        return 2 * 3.14 *r ;
+    }
+}
+  
+
+
+
