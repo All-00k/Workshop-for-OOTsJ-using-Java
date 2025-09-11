0089882467b65a485d9b8a5d5255f9ee37ca3926
All-00k <ay879018@gmail.com>
2025-09-11 14:44:32 +0530

    New hai ji

--- New.java
+++ New.java
@@ -1,8 +1,6 @@
-class New() {
-    void m() {
-        new Runnable() {
-            public void run() {
-            }
-        };
+class New {
+    public static void main(String[] args) {
+        System.out.println("NEW NEW HAI JI");
+        
     }
-}
+    }
