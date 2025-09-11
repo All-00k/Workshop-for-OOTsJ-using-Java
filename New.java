be3cd6085c69ed641dff0ce8e12d3129c69ccf23
All-00k <ay879018@gmail.com>
2025-09-11 15:42:50 +0530

    new sync

--- New.java
+++ New.java
@@ -2,6 +2,7 @@ class New {
     public static void main(String[] args) {
         System.out.println("NEW NEW HAI JI");
         System.out.println("ALOK");
+        System.out.println("For your eyes do you understand");
         
     }
     }
