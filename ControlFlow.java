7ba679a63ad1c810583dfc048371a07f47819d28
All-00k <ay879018@gmail.com>
2025-09-12 15:35:13 +0530

    phir se new new

---
+++ ControlFlow.java
@@ -0,0 +1,19 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+        
+    int x=11112005;
+    int p=0, i=0;
+    for( ;x>p;p+=5){
+        //p = p +5;
+        i+=1;
+
+        
+    }
+    System.out.println("Bade bhai it is cool: "+i);
+
+
+        
+
+    }
+    
+}
