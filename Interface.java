2686e1e40b76694954df40c378253169defbcac4
All-00k <ay879018@gmail.com>
2025-09-15 15:59:23 +0530

    interface

---
+++ Interface.java
@@ -0,0 +1,20 @@
+interface hello {
+    void print();
+}
+class Hi implements hello{
+    public void print(){
+        System.out.println("HEllo");
+    }
+    public static void main(String[] args) {
+        Hi obj = new Hi();
+        obj.print();
+    }
+}
+
+//interface can have only abstract methods. Since Java 8, it can have default and static methods
+//interafce is a type of abstract class which can implements multiple sub classes ("multiple inheritance")
+
+
+
+    
+
