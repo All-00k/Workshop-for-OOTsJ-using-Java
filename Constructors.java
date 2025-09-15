787e4b16741446a53eda4aeef62b77a03169b43d
All-00k <ay879018@gmail.com>
2025-09-15 14:44:31 +0530

    Default and Parameterized constructor

--- Constructors.java
+++ Constructors.java
@@ -2,11 +2,11 @@ public class Constructors {
     int id  ;
     String name;
     int age;
-    public Constructors(int i,String n){
+    public Constructors(int i,String n){   //first contructor
         id = i;
         name =n;
     }
-    public Constructors(int i, String n,int a){
+    public Constructors(int i, String n,int a){ //second constructor
         id =i;
         name =n;
         age =a;
@@ -26,6 +26,11 @@ public class Constructors {
 
 
     }
+
+//so basically the First constructor is called when we give 2 parameters to the constructor
+//and the below commstructor is called when we give 3 parameters to the constructor
+// what if we remove the constructor with 2 parameters then objectt s1 will show error "Constuctor undefined"
+
 
     
 
