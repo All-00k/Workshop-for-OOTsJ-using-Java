4d3198f3801e302ccabdc6b76c5f307aaba780ca
All-00k <ay879018@gmail.com>
2025-09-19 15:54:31 +0530

    Argumnets form command line

--- TakeInputFromArgs.java
+++ TakeInputFromArgs.java
@@ -1,7 +1,8 @@
 public class TakeInputFromArgs {
     public static void main(String[] args) {
         
-        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1])); 
+        //by default command line take string arguments to convert in into integer we have to do "Integer.parseInt(args[0])"
     }
     
 }
