46d6100915b7f585781bff16b887f2faa57dbb0a
All-00k <ay879018@gmail.com>
2025-09-20 10:17:58 +0530

    Use of Throws exception

---
+++ UserDemo2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class UserDemo2 {
+    
+
+    public static void main(String[] args) throws Exception{
+        int num1, num2, result;
+        
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            // InputStream isr = new InputStream(System.in);
+            // BufferedReader br = new BufferedReader(isr);
+            System.out.println("Enter first number: ");
+            num1 = Integer.parseInt(br.readLine());
+            System.out.println("Enter second number: ");
+            num2 = Integer.parseInt(br.readLine());
+            result= num1 + num2;
+            System.out.println("Sum: "+result);
+                
+        
+            
+            
+        }
+}
+
+  
+
